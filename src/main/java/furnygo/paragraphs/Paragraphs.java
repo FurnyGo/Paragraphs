cc3652ee97006c162c48efe4f9e1fa9d84879bcb
FurnyGo <68079109+FurnyGo@users.noreply.github.com>
2023-05-29 00:31:21 +0400

    Add files via upload

---
+++ src/main/java/furnygo/paragraphs/Paragraphs.java
@@ -0,0 +1,10 @@
+package furnygo.paragraphs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Paragraphs implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Paragraphs mod started!");
+	}
+}
